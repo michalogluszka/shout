c8161395de61903d254db64839326f208fb5e21d
Michal Ogluszka <michal.ogluszka@gmail.com>
2016-03-10 22:19:06 +0100

    not working json error

--- src/shout/Startup.cs
+++ src/shout/Startup.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
-using Microsoft.Framework.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace shout
 {
