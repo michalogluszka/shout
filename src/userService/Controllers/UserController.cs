53d9f9a4410b4e705e2e42eb7778e75453bc111d
Michal Ogluszka <michal.ogluszka@gmail.com>
2016-03-24 00:33:29 +0100

    JQuery call to ASP.NET MVC6 on Ubuntu

--- src/userService/Controllers/UserController.cs
+++ src/userService/Controllers/UserController.cs
@@ -4,14 +4,20 @@ using Microsoft.AspNet.Mvc;
 
 namespace Shout.UserService.Controllers
 {
+    class User
+    {
+        public string UserName { get; set;}
+    }
+    
     [Route("api/[controller]")]
     public class UserController : Controller
     {
         // GET: api/user
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
+        public JsonResult Get()
+        {            
+            var user = new User() { UserName = "michalogluszka" };
+            return Json(user);            
         }
     }
 }
--- src/userService/Startup.cs
+++ src/userService/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNet.Cors;
 
 namespace Shout.UserService
 {
@@ -8,12 +9,20 @@ namespace Shout.UserService
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+                                                                        .AllowAnyMethod()
+                                                                         .AllowAnyHeader()));                                                                                                                                                  
+            
             services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseCors("AllowAll");
             app.UseMvc();
+            
+            
         }
     }
 }
