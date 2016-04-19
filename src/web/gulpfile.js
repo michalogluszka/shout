1625b9f6e850d7abd3c6f7afcf1e471bb66f1a48
Michal Ogluszka <michal.ogluszka@gmail.com>
2016-04-19 23:34:20 +0200

    load plugins added

--- src/web/gulpfile.js
+++ src/web/gulpfile.js
@@ -1,41 +1,26 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var mainBowerFiles = require('gulp-main-bower-files');
-var plumber = require('gulp-plumber');
 var gulpDebug = require('gulp-debug');
+
+var publishFolder = 'public';
+var libsFolder = 'libs';
+
 
 gulp.task('default', function() {
   // place code for your default task here
   gutil.log('gulp started')  
 });
 
-gulp.task('copyHtmlandCSS', ['default'], function(copyHtml) {
+gulp.task('publish', ['default'], function() {
    
   // copy any html files in source/ to public/
   gutil.log('copying html files');
-  gulp.src('*.html').pipe(gulp.dest('dist'));
-  gutil.log('copying css')
-  //that's nasty
-  gulp.src('bower_components/bootstrap/dist/css/bootstrap.min.css').pipe(gulp.dest('dist/css'));
+  gulp.src('*.html').pipe(gulp.dest(publishFolder));  
+  
+  gulp.src('./bower.json')
+    .pipe(mainBowerFiles())
+    .pipe(gulpDebug({title:'debug-mainBowerFiles'}))
+    .pipe(gulp.dest(publishFolder))
+    .pipe(gulpDebug({title:publishFolder}));  
 });
-
-gulp.task('publish', function() {
-    return gulp.src('bower_components/**/*.css')
-    .pipe(gulp.dest('public'));
-});
-
-gulp.task('bower', function() {
-    return gulp.src('./bower.json')
-        .pipe(gulpDebug({title:'debug-src'}))
-        .pipe(mainBowerFiles( ))
-        .pipe(gulpDebug({title:'debug-mainBowerFiles'}))
-        .pipe(gulp.dest('temp'))
-        .pipe(gulpDebug({title:'dest'}));
-});
-
-gulp.task('failing', function() {
-    return gulp.src('./bower.json')
-        .pipe(mainBowerFiles( ))
-        .pipe(gulp.dest('temp'));
-});
-
