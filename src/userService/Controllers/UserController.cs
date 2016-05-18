b0cbd2f3c8af4508d3b8acbf6877d66de41d779e
Michal Ogluszka <michal.ogluszka@gmail.com>
2016-05-18 02:09:01 +0200

    ugly try

--- src/userService/Controllers/UserController.cs
+++ src/userService/Controllers/UserController.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using Microsoft.AspNet.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace Shout.UserService.Controllers
--- src/userService/Startup.cs
+++ src/userService/Startup.cs
@@ -1,27 +1,43 @@
-﻿using Microsoft.AspNet.Builder;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNet.Cors;
+using Microsoft.Extensions.Logging;
 
 namespace Shout.UserService
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IHostingEnvironment env)
+        {
+              var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath);
+                
+            Configuration = builder.Build();
+        }
+        
+        public IConfigurationRoot Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            
-            services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
-                                                                        .AllowAnyMethod()
-                                                                         .AllowAnyHeader()));                                                                                                                                                  
-            
-            services.AddMvc();
+            //services.UseMvc();            
         }
-
-        public void Configure(IApplicationBuilder app)
+    
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            app.UseCors("AllowAll");
-            app.UseMvc();
-            
+            // app.UseMvc(routes =>
+            // {
+            //     routes.MapRoute(
+            //         name: "default",
+            //         template: "{controller=Home}/{action=Index}/{id?}");
+            // });
             
         }
     }
